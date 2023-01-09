a4a2647816d783af8fbac6637f5d6a8f284d0dbf
Chris <skoponer@gmail.com>
2023-01-09 15:07:03 +0100

    Added build-prod to packages.json

--- scripts/UpdateRemoteDatabase.ps1
+++ scripts/UpdateRemoteDatabase.ps1
@@ -3,4 +3,4 @@ Push-Location -Path $PSScriptRoot\..\backend\LoanComparer.Application
 dotnet ef database update --startup-project ..\LoanComparer.Api --connection 'Server=tcp:loancomparerserverpw.database.windows.net,1433;Initial Catalog=LoanComparerDB;Persist Security Info=False;User ID=LoanComparerAdmin;Password=LoanComparerRules!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;'
 dotnet ef database update --startup-project ..\LoanComparer.Api
 
-Pop-Locationcd
+Pop-Location
