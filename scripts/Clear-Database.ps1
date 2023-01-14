0685fcab7dd9b3e911c00d981eb8fa9763772200
Meeso1 <kubaknyspel@gmail.com>
2023-01-14 12:14:26 +0100

    Fixed Clear-Database script.

--- scripts/Clear-Database.ps1
+++ scripts/Clear-Database.ps1
@@ -2,6 +2,5 @@ Push-Location -Path $PSScriptRoot\..\backend\LoanComparer.Application
 
 dotnet ef database update 0 --startup-project ..\LoanComparer.Api --connection 'Server=localhost;Integrated Security=true;Database=LoanComparerTest'
 dotnet ef database update --startup-project ..\LoanComparer.Api --connection 'Server=localhost;Integrated Security=true;Database=LoanComparerTest'
-dotnet ef database update --startup-project ..\LoanComparer.Api
 
 Pop-Location
